d031b3fe5ec9f0b4794fd1ef58a82335cbb45b84
Nguyễn Thị Hoa (429606) <hoant239@viettelsoftware.com>
2025-07-15 16:09:20 +0700

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,30 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Image, SimpleGrid, Text } from "@chakra-ui/react";
 
 export default function Footer() {
+  const navLinkList = [
+    {
+      name: "Home",
+      url: "/",
+    },
+    {
+      name: "Specials",
+      url: "",
+    },
+    {
+      name: "CustomersSay",
+      url: "",
+    },
+    {
+      name: "Chicago",
+      url: "",
+    },
+    {
+      name: "BookingPage",
+      url: "/booking",
+    },
+  ];
   return (
-    <>
+    <Box marginTop={90} backgroundColor="#495e57">
       <Flex
         margin="0 auto"
         justifyContent="space-between"
@@ -11,8 +33,78 @@ export default function Footer() {
         height={406}
         position={"relative"}
       >
-
+        <SimpleGrid
+          columns={{ base: 1, md: 3, lg: 4 }}
+          spacing={6}
+          mt={8}
+          maxW="1024px"
+          margin="30px 0"
+        >
+          <Box>
+            <Image src="../images/Logo.svg" height={50} width={"fit-content"} />
+            <Text fontSize={32} fontWeight={500}>
+              Little Lemon Chicago
+            </Text>
+          </Box>
+          <Box>
+            <Flex margin="0 auto" alignItems="left" direction={"column"}>
+              <Text fontSize={18} color={"#facc3d"} fontWeight={500}>
+                Doormat Navigation
+              </Text>
+              <ul className="navbar" style={{ flexDirection: "column" }}>
+                {navLinkList.map((item) => {
+                  return (
+                    <li>
+                      <a href={item.url} color="black">
+                        {item.name}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </Flex>
+          </Box>
+          <Box>
+            <Flex margin="0 auto" alignItems="left" direction={"column"}>
+              <Text fontSize={18} color={"#facc3d"} fontWeight={500}>
+                Contact
+              </Text>
+              <ul className="navbar" style={{ flexDirection: "column" }}>
+                <li>
+                  <a color="black">123 Main St.</a>
+                </li>
+                <li>
+                  <a color="black">(123) 456-7890</a>
+                </li>
+                <li>
+                  <a color="black">information@littlelemon.com</a>
+                </li>
+              </ul>
+            </Flex>
+          </Box>
+          <Box>
+            <Flex margin="0 auto" alignItems="left" direction={"column"}>
+              <Text fontSize={18} color={"#facc3d"} fontWeight={500}>
+                Doormat Navigation
+              </Text>
+              <ul
+                className="navbar"
+                style={{ flexDirection: "column", gap: 5 }}
+              >
+                <li>
+                  <a color="black">Instagram</a>
+                </li>
+                <li>
+                  <a color="black">Facebook</a>
+                </li>
+                <li>
+                  <a color="black">Twitter</a>
+                </li>
+              </ul>
+            </Flex>
+          </Box>
+        </SimpleGrid>
       </Flex>
-    </>
+    </Box>
   );
 }
